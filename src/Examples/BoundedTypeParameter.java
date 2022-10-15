360ef5eef332e287de2eb8bec51c71c30b468b6d
VijayHYadav <vizzyadav1997@gmail.com>
2022-10-15 07:50:02 +0530

    0009_Bounded_Type_Parameters

---
+++ src/Examples/BoundedTypeParameter.java
@@ -0,0 +1,38 @@
+package Examples;
+
+public class BoundedTypeParameter {
+	public static void main(String[] args) {
+		
+		Integer[] numbers = {1,2,3};
+		Double[] doubleNumbers = {1.0, 2.0, 3.0};
+
+		System.out.println(
+				countGreaterThen(numbers, 1)
+		);
+
+		System.out.println(
+				countGreaterThen(doubleNumbers, 2.0)
+		);
+	}
+	
+	// Generic Bounded Type Parameters
+	static <T extends Comparable> int countGreaterThen(T[] numbers, T number) {
+		int count = 0;
+		for (T n: numbers) {
+			if (n.compareTo(number) > 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+//	static int countGreaterThen(int[] numbers, int number) {
+//		int count = 0;
+//		for (int n : numbers) {
+//			if (n > number) {
+//				count++;
+//			}
+//		}
+//		return count;
+//	}
+}
